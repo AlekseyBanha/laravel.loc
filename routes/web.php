33bdcad2a7cf3d9526f5c8211b9fcf8b3007a3e7
bandito228 <92264227+bandito228@users.noreply.github.com>
2021-11-12 16:17:11 +0200

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+                                //Урок 5//
+//Route::match(['post','get'],'sendMail',function (){
+//
+//
+//    if (!empty($_POST)){
+//        dump($_POST);
+//   }
+//    return view('contact');
+//})->name('sendMail');
+//Route::get('SendMail',function (){
+//        return view('contact');
+//});
+//Route::post('/Send',function (){
+//    if(!empty($_POST)){
+//        dd($_POST);
+//    }
+//});                   //Урок 6//
+//Route::match(['get','post'],'SendMail',function (){
+//
+//        if (!empty($_POST)){
+//            dump($_POST);
+//        }
+//        return view('contact');
+//});
+
+//Route::get('/post/edit/{id?}',function ($id=null){
+//    return "Edit Post $id";
+//})->where('id','[0-9]+')->name('create');
+
+Route::fallback(function ())
